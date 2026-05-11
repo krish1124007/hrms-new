838c04d1b9425c82845036a1501c61e662f645d5
krish1124007 <parmarkrish005@gmail.com>
2026-05-11 23:14:22 +0530

    update the live url

--- frontend/dist/assets/skeleton-BwOwX5OP.js
+++
@@ -1,2 +0,0 @@
-import{j as t,i as n}from"./index-BiWWWL1I.js";function m({className:e,...s}){return t.jsx("div",{className:n("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
-//# sourceMappingURL=skeleton-BwOwX5OP.js.map
