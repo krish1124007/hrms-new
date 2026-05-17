7c8f2e6484a7babf87bb594a3e720d5b11c289ef
krish1124007 <parmarkrish005@gmail.com>
2026-05-17 10:40:01 +0530

    update the all the counts

--- frontend/dist/assets/skeleton-QHgQOdnr.js
+++
@@ -1,2 +0,0 @@
-import{j as t,i as n}from"./index-G_-as30X.js";function m({className:e,...s}){return t.jsx("div",{className:n("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
-//# sourceMappingURL=skeleton-QHgQOdnr.js.map
