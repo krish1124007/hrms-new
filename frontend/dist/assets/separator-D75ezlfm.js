62f7984654c9f9096011f44c5c6aeab71ec36b79
krish1124007 <parmarkrish005@gmail.com>
2026-06-02 00:22:07 +0530

    update the all new changes

--- frontend/dist/assets/separator-D75ezlfm.js
+++
@@ -1,2 +0,0 @@
-import{j as a,i as s}from"./index-MtgZHrpI.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
-//# sourceMappingURL=separator-D75ezlfm.js.map
