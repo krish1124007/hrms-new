f43ccfb873da9fd8fe16938a3b73fa2782ac6d29
krish1124007 <parmarkrish005@gmail.com>
2026-06-27 13:18:24 +0530

    Update the New Frontend

--- frontend/dist/assets/separator-BLpqI9dC.js
+++
@@ -1,2 +0,0 @@
-import{j as a,i as s}from"./index-UhCS02ge.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
-//# sourceMappingURL=separator-BLpqI9dC.js.map
---
+++ frontend/dist/assets/separator-DXq0N4tp.js
@@ -0,0 +1,2 @@
+import{j as a,i as s}from"./index-CAlwnwAG.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
+//# sourceMappingURL=separator-DXq0N4tp.js.map
