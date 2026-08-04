89c4aec96a9efa97c124c1e99a5d854cf85c87bb
krish1124007 <parmarkrish005@gmail.com>
2026-08-04 11:13:57 +0530

    update the changes

---
+++ frontend/dist/assets/separator-C_WJXK0-.js
@@ -0,0 +1,2 @@
+import{j as a,k as s}from"./index-CRc9lMNr.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
+//# sourceMappingURL=separator-C_WJXK0-.js.map
