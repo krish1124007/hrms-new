8d2b98143a5dcbcc3b5892a232c20088f474529a
krish1124007 <parmarkrish005@gmail.com>
2026-08-05 08:30:14 +0530

    update the id card detail

--- frontend/dist/assets/separator-D9I7_s2q.js
+++
@@ -1,2 +0,0 @@
-import{j as a,k as s}from"./index-04lQpks5.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
-//# sourceMappingURL=separator-D9I7_s2q.js.map
---
+++ frontend/dist/assets/separator-Dg1JqyQo.js
@@ -0,0 +1,2 @@
+import{j as a,k as s}from"./index-BQgxEqTM.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
+//# sourceMappingURL=separator-Dg1JqyQo.js.map
