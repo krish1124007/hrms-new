d02ecfa9d4540a11fc795e835e1896dc36d3d6ad
krish1124007 <parmarkrish005@gmail.com>
2026-08-12 11:09:15 +0530

    update the site

---
+++ frontend/dist/assets/skeleton-02frOfot.js
@@ -0,0 +1,2 @@
+import{j as t,q as n}from"./index-D-YUObtW.js";function m({className:e,...s}){return t.jsx("div",{className:n("animate-pulse rounded-md bg-muted",e),...s})}export{m as S};
+//# sourceMappingURL=skeleton-02frOfot.js.map
