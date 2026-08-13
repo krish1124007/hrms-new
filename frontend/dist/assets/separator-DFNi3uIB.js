9197d2aeb8fc63812078094b664bc4977618a8e9
krish1124007 <parmarkrish005@gmail.com>
2026-08-13 07:28:49 +0530

    update the data

--- frontend/dist/assets/separator-DFNi3uIB.js
+++
@@ -1,2 +0,0 @@
-import{j as a,q as s}from"./index-CF_xKW_u.js";function e({className:r,orientation:o="horizontal"}){return a.jsx("div",{role:"separator",className:s("shrink-0 bg-border",o==="horizontal"?"h-px w-full":"h-full w-px",r)})}export{e as S};
-//# sourceMappingURL=separator-DFNi3uIB.js.map
